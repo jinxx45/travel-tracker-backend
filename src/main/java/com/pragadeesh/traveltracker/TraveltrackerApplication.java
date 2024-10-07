a5b98e42157d7eb0811d93bd9e19a86589b3e7ef
Pragadeesh <www.pragat7@gmail.com>
2024-10-07 16:42:13 +0530

    fetch and save API for Footprints

---
+++ src/main/java/com/pragadeesh/traveltracker/TraveltrackerApplication.java
@@ -0,0 +1,13 @@
+package com.pragadeesh.traveltracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraveltrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraveltrackerApplication.class, args);
+	}
+
+}
